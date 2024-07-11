e4be64083fa6146ba23d7e0e5999d90584d6afcc
danyalutsevich <luchevich31@gmail.com>
2024-07-12 00:49:16 +0300

    my config

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+      config.setup({
+  ensure_installed={
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "yaml",
+        "css",
+        "html",
+        "json5",
+        "bash",
+        "scss",
+        "markdown",
+        "dockerfile",
+        "python",
+        "typescript",
+        "go",
+      },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+
+  end
+}
+
