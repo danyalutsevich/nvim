b0edb54d7a661fd8db4ce565131ebde669663cb3
danyalutsevich <luchevich31@gmail.com>
2024-07-13 16:35:58 +0300

    tmux navigator

--- init.lua
+++ init.lua
@@ -7,5 +7,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
--- vim.cmd("set termguicolors")
+vim.cmd("set clipboard=unnamed")
+
+-- vim.keymap.set('n','<c-k>',':wincmd k<CR>')
+-- vim.keymap.set('n','<c-j>',':wincmd j<CR>')
+-- vim.keymap.set('n','<c-h>',':wincmd h<CR>')
+-- vim.keymap.set('n','<c-l>',':wincmd l<CR>')
 
