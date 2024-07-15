b9957770b81b6527f14472d9194d93afb1b06468
danyalutsevich <luchevich31@gmail.com>
2024-07-15 22:41:14 +0300

    remove unused themes

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -2,19 +2,19 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-		function tprint(tbl, indent)
+		function Tprint(tbl, indent)
 			if not indent then
 				indent = 0
 			end
 			for k, v in pairs(tbl) do
-				formatting = string.rep("  ", indent) .. k .. ": "
+				Formatting = string.rep("  ", indent) .. k .. ": "
 				if type(v) == "table" then
-					print(formatting)
-					tprint(v, indent + 1)
+					print(Formatting)
+					Tprint(v, indent + 1)
 				elseif type(v) == "boolean" then
-					print(formatting .. tostring(v))
+					print(Formatting .. tostring(v))
 				else
-					print(formatting .. v)
+					print(Formatting .. v)
 				end
 			end
 		end
